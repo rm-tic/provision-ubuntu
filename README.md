8328a641198036abbc67f7ed0ba250f0948934af
Rodrigo Martins <rodrigomartins.tic@gmail.com>
2020-07-12 20:22:45 -0300

    Init Project

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# provision-ubuntu
-Ansible Provision - Distro Ubuntu
+# Ansible Provision Workstation Ubuntu
+
+<p align="center">
+  <img src="img/ubuntu_logo.png">
+</p>
+
+#### Distro suportada: Ubuntu 20.04
+
+Esta receita provisiona uma estação de trabalho recém formatada com Ubuntu 20.04
+
+- Instala diversos pacotes GUI/CLI
+- Arquivos de configuração customizados
+- Temas / Terminal customizados
+
+
+<br>
+<br>
+
+**SHELL**
+```bash
+#CURL
+curl -fsSL https://raw.githubusercontent.com/rm-tic/provision-ubuntu/master/provision_ubuntu_init.sh | bash -
+
+#WGET
+wget -qO- https://raw.githubusercontent.com/rm-tic/provision-ubuntu/master/provision_ubuntu_init.sh | bash -
+
+```
+
+
---
+++ provision_ubuntu_init.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+
+function GTERM_LOAD()
+{
+	GTERM_DIR="$HOME/.gterm"
+	GTERM_FILE="$GTERM_DIR/gterm-profile.dconf"
+  GTERM_RC="$GTERM_DIR/gterm.rc"
+	GTERM_URL="https://raw.githubusercontent.com/rm-tic/provision-ubuntu/master/gterm-profile.dconf"
+
+
+	if [[ ! -d $GTERM_DIR  || "$(cat $GTERM_RC)" != "0" ]]; then
+
+		mkdir $GTERM_DIR
+    echo "0" > $GTERM_RC
+
+		wget -qO $GTERM_FILE $GTERM_URL
+
+		#DCONF EXPORT
+		#dconf dump /org/gnome/terminal/legacy/profiles:/ > $GTERM_FILE
+		
+		#DCONF IMPORT
+		dconf load /org/gnome/terminal/legacy/profiles:/ < $GTERM_FILE
+
+	fi
+
+}
+
+
+function CHECK_INSTALL()
+{
+
+	PKG="$(dpkg-query -W -f='${db:Status-Abbrev}' $1 2> /dev/null | cut -c 1-2)"
+
+	if [ "$PKG" = "ii" ]; then
+
+		echo "present"
+
+	else
+
+		echo "absent"
+
+	fi
+
+}
+
+function APT_UPDATE()
+{
+
+	if [ "$UPDATE_RC" != "0" ]; then
+
+		sudo apt-get update -qq
+		UPDATE_RC="0"
+
+	fi
+
+}
+
+
+function INSTALL()
+{
+
+	GIT_STATUS="$(CHECK_INSTALL git)"
+	PYTHON3_VENV_STATUS="$(CHECK_INSTALL python3-venv)"
+  PYTHON3_PIP_STATUS="$(CHECK_INSTALL python3-pip)"
+
+	echo "Installing Essential Packages..."
+	echo
+
+
+	if [ "$PYTHON3_VENV_STATUS" = "absent" ]; then
+
+    APT_UPDATE
+    sudo apt-get install -y python3-venv > /dev/null 2>&1
+		echo ">> python3-venv installed."
+
+	else
+
+		echo ">> python3-venv is already installed."
+
+	fi
+
+  if [ "$PYTHON3_PIP_STATUS" = "absent" ]; then
+
+    APT_UPDATE
+    sudo apt-get install -y python3-pip > /dev/null 2>&1
+		echo ">> python3-pip installed."
+
+	else
+
+		echo ">> python3-pip is already installed."
+
+	fi
+
+	if [ "$GIT_STATUS" = "absent" ]; then
+
+		APT_UPDATE
+    sudo apt-get install -y git > /dev/null 2>&1
+		echo ">> Git v$(git --version | awk '{print $3}') installed."
+
+	else
+
+		echo ">> Git is already installed."
+
+	fi
+
+
+	echo
+
+}
+
+
+function CLONE_REPO()
+{
+	REPO_DIR="/tmp/$$/provision-ubuntu"
+	REPO_URL="https://github.com/rm-tic/provision-ubuntu.git"
+
+	echo "Cloning Repository in $REPO_DIR"
+
+	git clone $REPO_URL $REPO_DIR > /dev/null 2>&1
+
+	echo
+}
+
+function ENABLE_VENV()
+{
+  source $REPO_DIR/.venv/bin/active
+}
+
+function DISABLE_VENV()
+{
+  deactivate
+}
+
+function SETUP_VENV()
+{
+
+  python3 -m venv $REPO_DIR/.venv
+  ENABLE_VENV
+  python3 -m pip install --upgrade pip
+  python3 -m pip install ansible
+  DISABLE_VENV
+
+}
+
+
+function EXEC_ANSIBLE()
+{
+	echo
+	echo "Starting Playbook..."
+	echo
+	
+  ENABLE_VENV
+	sudo ansible-playbook -i $REPO_DIR/hosts $REPO_DIR/main.yml
+}
+
+
+function MAIN()
+{
+
+echo
+echo "+----------------------------------+"
+echo "| Invencible (Ansible)             |"
+echo "| Project: provision-ubuntu        |"
+echo "| Author: Rodrigo Martins (IceTux) |"
+echo "| Creation Date: 2020-07-12        |"
+echo "+----------------------------------+"
+echo
+echo
+
+#Exec Functions
+GTERM_LOAD
+INSTALL
+CLONE_REPO
+SETUP_VENV
+EXEC_ANSIBLE
+
+}
+
+MAIN
